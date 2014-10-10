bef5efbe00d1bea27aea0a577edbeea4e8473f12
Grégoire ROCHER <gr@enix.org>
2014-10-10 16:57:05 +0200

    Add shortcuts for table styles

--- pdf_generator/table.py
+++ pdf_generator/table.py
@@ -72,16 +72,22 @@ class Styles(object):
         'valign': 3,
     }
 
+    def __init__(self, *args):
+        self.args = args
+
     def __getattr__(self, name):
-        return CellsStyle(name, self.known_styles.get(name.lower()))
+        return CellsStyle(name, self.known_styles.get(name.lower()), self.args)
 
 
 class CellsStyle(object):
-    def __init__(self, name, argc):
+    def __init__(self, name, argc, args):
         self.name = name.upper()
         self.argc = argc
+        self.args = args
 
     def __call__(self, *args):
+        args = self.args + args
+
         if self.argc is not None and len(args) != self.argc:
             raise TypeError('{2}: Expecting {0} arguments, got {1}'.format(
                 self.argc, len(args), self.name))
@@ -89,3 +95,16 @@ class CellsStyle(object):
 
 
 styles = Styles()
+styles.all = Styles((0, 0), (-1, -1))
+
+styles.first_row = Styles((0, 0), (-1, 0))
+styles.first_rows = lambda x: Styles((0, 0), (-1, x))
+
+styles.last_row = Styles((0, -1), (-1, -1))
+styles.last_rows = lambda x: Styles((0, -x), (-1, -1))
+
+styles.first_col = Styles((0, 0), (0, -1))
+styles.first_cols = lambda x: Styles((0, 0), (x, -1))
+
+styles.last_col = Styles((-1, 0), (-1, -1))
+styles.last_cols = lambda x: Styles((-x, 0), (-1, -1))
